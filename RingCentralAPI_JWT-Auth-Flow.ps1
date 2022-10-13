fc4b14e208dbe63a1007e9a4347ce632c85bb90d
jeremyaelliott <jeremyaelliott@gmail.com>
2022-10-12 22:28:00 -0700

    Add files via upload

---
+++ RingCentralAPI_JWT-Auth-Flow.ps1
@@ -0,0 +1,68 @@
+<# 
+.SYNOPSIS
+    RingCentral API Query using "JWT auth flow" in Powershell
+
+.DESCRIPTION 
+    This script gets an authorization token that you use with API Queries
+ 
+.NOTES 
+    This requires an App on https://developers.ringcentral.com with the authentication method "JWT auth flow"
+
+.LINK 
+    Useful Link to ressources or others.
+
+.Parameter JWT
+    JWT Credential from https://developers.ringcentral.com/console/my-credentials
+
+.Parameter Server 
+    API Server URL. Used to switch between the Sandbox and Production Environment.
+#>
+
+#RingCentrals "JWT auth flow"
+#Change these values 
+$clientID     = ''
+$clientSecret = ''
+$JWT          = ''
+$server       = 'https://platform.devtest.ringcentral.com/'
+###################################################################################################################
+#Gets authorization token that you use with API Queries
+$clientCredsPlainText = '{0}:{1}' -f $clientID,$clientSecret
+$clientCredsBase64 = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($clientCredsPlainText))
+
+$invokeRestMethodSplat = @{
+    Uri         = '{0}/restapi/oauth/token' -f $server 
+    ContentType = 'application/x-www-form-urlencoded'
+    Body        = @{
+                    grant_type = 'urn:ietf:params:oauth:grant-type:jwt-bearer'
+                    assertion = $JWT
+    }
+    Headers     = @{
+                    Authorization = 'Basic {0}' -f $ClientCredsBase64
+    }
+    Method      = 'Post'
+}
+$token = Invoke-RestMethod @invokeRestMethodSplat
+$authorizationToken = '{0} {1}' -f $token.token_type,$token.access_token
+###################################################################################################################
+Function Get-APIQuery{
+    param(
+        $URI, 
+        $authorization
+    )
+    $query = Invoke-RestMethod -Uri $URI -Headers @{'Authorization' = $authorization}
+    $totalPages = $query.paging.totalPages
+    For ($page=1; $page -le $totalPages; $page++) {
+	    $query = Invoke-RestMethod -Uri ('{0}?page={1}' -f $URI,$page) -Headers @{'Authorization' = $authorization}
+        $query.records
+    }
+}
+###################################################################################################################
+#Query Example
+$getAPIQuerySplat= @{
+    URI = '{0}/restapi/v1.0/account/~/extension' -f $server
+    Authorization = $authorizationToken
+}
+$extensionRecords = Get-APIQuery @getAPIQuerySplat
+
+#Output Example
+$extensionRecords | Select-Object name,type,@{n='extension';e='extensionNumber'},@{n='email';e={$_.contact.email}} | Sort-Object type,name
